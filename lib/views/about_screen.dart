da93b8bc9ead133ec4da8619f075fde0004814d7
Untitled Programmer <blairlucas448@gmail.com>
2025-12-04 13:37:36 +0000

    Refactor AboutScreen to use Header for app bar and drawer navigation

--- lib/views/about_screen.dart
+++ lib/views/about_screen.dart
@@ -1,29 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:sandwich_shop/views/app_styles.dart';
+import 'package:sandwich_shop/views/header.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final bool isWide = MediaQuery.of(context).size.width >= 600;
+    final Map<String, String> routeLabels = {
+      '/': 'Home',
+      '/about': 'About',
+      '/login': 'Account',
+    };
+
+    final Widget content = const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Welcome to Sandwich Shop!', style: heading2),
+          SizedBox(height: 20),
+          Text(
+            'We are a family-owned business dedicated to serving the best sandwiches in town. ',
+            style: normalText,
+          ),
+        ],
+      ),
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('About Us', style: heading1),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Welcome to Sandwich Shop!', style: heading2),
-            SizedBox(height: 20),
-            Text(
-              'We are a family-owned business dedicated to serving the best sandwiches in town. ',
-              style: normalText,
-            ),
-          ],
-        ),
-      ),
+      appBar: Header.buildAppBar(context, title: 'About Us'),
+      drawer:
+          isWide ? null : Header.buildDrawer(context, routeLabels: routeLabels),
+      body: isWide
+          ? Row(
+              children: [
+                Header.buildSideNav(context, routeLabels: routeLabels),
+                Expanded(child: content),
+              ],
+            )
+          : content,
     );
   }
 }
